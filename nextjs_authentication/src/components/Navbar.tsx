6e37b24e302354644a11655fd43058a266c95949
prashantbhoite123 <pbhoite985@gmail.com>
2024-12-08 20:11:44 +0530

    complete next authentication with github

--- nextjs_authentication/src/components/Navbar.tsx
+++ nextjs_authentication/src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
+import { auth, signOut, signIn } from "../../auth"
+import { redirect } from "next/dist/server/api-utils"
 
-function Navbar() {
+const Navbar = async () => {
+  const session = await auth()
   return (
-    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+    <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
           <Image
@@ -13,8 +16,40 @@ function Navbar() {
             height={30}
           />
         </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server"
+                  await signOut({
+                    redirectTo: "/",
+                    redirect: true,
+                  })
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+              <Link href={`/user/${session?.user?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server"
+                await signIn()
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
       </nav>
-    </header>
+    </div>
   )
 }
 
