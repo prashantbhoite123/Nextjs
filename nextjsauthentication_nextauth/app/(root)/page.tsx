aa5c93aabf692154946c8e087a2c58d1a5ce7921
prashantbhoite123 <pbhoite985@gmail.com>
2024-12-16 19:36:33 +0530

    complete card ui sectino

--- nextjsauthentication_nextauth/app/(root)/page.tsx
+++ nextjsauthentication_nextauth/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import SearchForm from "../../components/SearchForm"
+import StartupCard from "@/components/StartupCard"
 
 const Home = async ({
   searchParams,
@@ -7,6 +8,20 @@ const Home = async ({
   searchParams: Promise<{ query?: string }>
 }) => {
   const query = (await searchParams).query
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      Views: 55,
+      author: { _id: 1, name: "Prashant Bhoite" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://images.pexels.com/photos/2548535/pexels-photo-2548535.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ]
   return (
     <>
       <section className="pink_container">
@@ -18,6 +33,20 @@ const Home = async ({
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competition{" "}
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   )
--- nextjsauthentication_nextauth/lib/utils.ts
+++ nextjsauthentication_nextauth/lib/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from "clsx"
+import exp from "constants"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: string) {
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  })
 }
