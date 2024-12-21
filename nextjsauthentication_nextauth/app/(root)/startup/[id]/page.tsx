5160f4972d5b7e71d7d38cfc98c796e5e73034e6
prashantbhoite123 <pbhoite985@gmail.com>
2024-12-21 22:03:05 +0530

    create detail page api

--- nextjsauthentication_nextauth/app/(root)/startup/[id]/page.tsx
+++ nextjsauthentication_nextauth/app/(root)/startup/[id]/page.tsx
@@ -1,8 +1,19 @@
+import { client } from "@/sanity/lib/client"
+import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries"
+import { notFound } from "next/navigation"
+
+export const axperimental_ppr = true
+
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id
+
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id })
+
+  if (!post) return notFound()
+
   return (
     <>
-      <h1 className="text-3xl font-semibold">this is a startup number {id}</h1>
+      <h1 className="text-3xl font-semibold">{post.title}</h1>
     </>
   )
 }
