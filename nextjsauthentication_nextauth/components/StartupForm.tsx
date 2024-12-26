07dca96a3d0dd64fdac8eb8a745930443ac76c50
prashantbhoite123 <pbhoite985@gmail.com>
2024-12-26 22:44:31 +0530

    complete cretae page

--- nextjsauthentication_nextauth/components/StartupForm.tsx
+++ nextjsauthentication_nextauth/components/StartupForm.tsx
@@ -10,6 +10,7 @@ import { z } from "zod"
 import { formSchema } from "@/lib/validation"
 import { useToast } from "@/hooks/use-toast"
 import { useRouter } from "next/router"
+import { createPitch } from "@/lib/actions"
 
 const StartupForm = () => {
   const router = useRouter()
@@ -32,16 +33,16 @@ const StartupForm = () => {
 
       console.log(formValues)
 
-      // const result = await createIdea(prevState, formData, pitch)
+      const result = await createPitch(prevState, formData, pitch)
 
-      // if (result.status === "SUCCESS") {
-      //   toast({
-      //     title: "Success",
-      //     description: "Your startup pitch has been created Successfully",
-      //   })
-      // router.push(`/startup/${result.id}`)
-      // }
-      // return result
+      if (result.status === "SUCCESS") {
+        toast({
+          title: "Success",
+          description: "Your startup pitch has been created Successfully",
+        })
+        router.push(`/startup/${result._id}`)
+      }
+      return result
     } catch (error) {
       if (error instanceof z.ZodError) {
         const firldErorrs = error.flatten().fieldErrors
